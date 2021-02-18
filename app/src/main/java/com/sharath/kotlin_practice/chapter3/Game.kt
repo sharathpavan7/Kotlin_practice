d80f9ddebc02116cd67916ad4a670a65744a6b2c
Sharath7 <sharathpavan7@gmail.com>
2021-02-18 22:07:14 +0530

    chapter 3 completed.

--- app/src/main/java/com/sharath/kotlin_practice/chapter3/Game.kt
+++ app/src/main/java/com/sharath/kotlin_practice/chapter3/Game.kt
@@ -9,7 +9,50 @@ import kotlin.random.Random
 fun main(args: Array<String>) {
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
-    println(gameChoice)
+    //println(gameChoice)
+    val userChoice = getUserChoice(options)
+
+    // showing final result
+    showFinalResult(userChoice, gameChoice)
 }
 
 private fun getGameChoice(optionParams: Array<String>) = optionParams[Random.nextInt(0, optionParams.size)]
+
+private fun getUserChoice(optionParams: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+
+    // looping untill the user enters a valid option
+    while (!isValidChoice) {
+        // ask the user for their choice
+        print("Please enter one of the following:")
+        for (option in optionParams) print(" $option")
+        println(".")
+
+        var userInput = readLine()
+        userInput = userInput?.capitalize()
+        // user has entered a valid option
+        if (userInput != null && userInput in optionParams) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+
+        // user entered a invalide input
+        if (!isValidChoice) println("You have entered an invalid option.")
+    }
+    return userChoice
+}
+
+private fun showFinalResult(userChoice: String, gameChoice: String) {
+
+    val result: String
+    // figurout the result
+    if (userChoice.equals(gameChoice, true)) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Paper" && gameChoice == "Rock") ||
+        (userChoice == "Scissors" && gameChoice == "Paper")) result = "You won"
+    else result = "You lost"
+
+    // showing final result
+    println("Your choice: $userChoice, game choice: $gameChoice. $result")
+}
